c80d6137922e1de8f19cff64abe75603b42626ee
Michael A. Floyd <mafloyd23@gmail.com>
2019-01-09 21:26:05 -0600

    Working through Chapter 10.

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ class Config(object):
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.googlemail.com'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
 
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
 
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
     
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 
