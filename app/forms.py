a808986367422cc79552de1ae3e2530ca9153322
Michael A. Floyd <mafloyd23@gmail.com>
2019-01-09 20:15:19 -0600

    Follow and unfollow is implemented now. Also, through Chapter 9;

--- app/forms.py
+++ app/forms.py
@@ -18,12 +18,10 @@ class RegistrationForm(FlaskForm):
     password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
-
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError('Please use a different username.')
-
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
@@ -40,10 +38,14 @@ class EditProfileForm(FlaskForm):
         super(EditProfileForm, self).__init__(*args, **kwargs)
         self.original_username = original_username
 
-
     def validate_username(self, username):
         if username.data != self.original_username:
             user = User.query.filter_by(username=self.username.data).first()
 
             if user is not None:
                 raise ValidationError('Please user a different username.')
+
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
